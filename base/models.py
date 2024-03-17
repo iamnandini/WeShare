44236cc7e74c396b966cabf4e7148b07e1a89d74
Nandini Soni <117187906+iamnandini@users.noreply.github.com>
2024-03-17 12:10:49 +0530

    "Some Basic changes"

--- base/models.py
+++ base/models.py
@@ -3,12 +3,13 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     name = models.CharField(max_length=200, null=True)
+    username = models.CharField(max_length=200,unique=True, null=False)
     email = models.EmailField(unique=True, null=True)
     bio = models.TextField(null=True)
 
     avatar = models.ImageField(null=True, default="avatar.svg")
 
-    USERNAME_FIELD = 'email'
+    USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = ['name']
 
 class Topic(models.Model):
